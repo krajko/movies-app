565fb537d7ee023d68590a113c97d327a7d4041f
krajko <nikolakrajinovic99@gmail.com>
2021-06-11 01:04:59 +0200

    added movie-card component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card col-10 col-sm-8 col-md-5 col-lg-4 col-xl-3 text-start m-3 p-0">
+        <img :src="movie.imageUrl" class="card-img-top" alt="Movie poster">
+
+        <div class="card-body d-flex align-items-center pb-0">
+            <h4 class="card-title"><strong>{{ movie.title }} ({{ movie.releaseDate | getYear }})</strong></h4> 
+        </div>
+
+        <div class="card-footer d-flex justify-content-between py-3">
+            <small>{{ movie.director}}</small>
+            <small>Genre: <span class="text-primary">{{ movie.genre }}</span></small>
+        </div>                   
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'movie-list',
+
+    props: {
+        movie: Object
+    },
+
+    filters: {
+        getYear(date) {
+            const newDate = new Date(date);
+            return newDate.getFullYear();
+        }
+    }
+}
+</script>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,24 +1,23 @@
 <template>
     <div>
-        <b-form-input v-model="query" autocomplete="off" class="col rounded-pill mx-auto mt-4 mb-3 px-3" placeholder="Start typing to filter..." style="width: 300px"/>
+        <b-form-input 
+            v-model="query" 
+            autocomplete="off" 
+            class="col rounded-pill mx-auto mt-4 mb-3 px-3" 
+            style="width: 300px"
+            placeholder="Start typing to filter..."
+        />
 
         <div class="d-flex flex-row flex-wrap justify-content-center">
-
             <p v-if="movies.length === 0 && query">No movie title matches the search term.</p>
 
-            <router-link :to="`/movies/${movie.id}`" v-for="movie in filteredMovies" :key="movie.id" class="card col-10 col-sm-8 col-md-5 col-lg-4 col-xl-3 text-start m-3 p-0">                    
-                <img :src="movie.imageUrl" class="card-img-top" alt="Movie poster">
-
-                <div class="card-body d-flex align-items-center pb-0">
-                    <h4 class="card-title"><strong>{{ movie.title }} ({{ movie.releaseDate | getYear }})</strong></h4> 
-                </div>
-
-                <div class="card-footer d-flex justify-content-between py-3">
-                    <small>{{ movie.director}}</small>
-                    <small>Genre: <span class="text-primary">{{ movie.genre }}</span></small>
-                </div>
-            </router-link>
-
+            <movie-card 
+                v-for="movie in filteredMovies" 
+                :key="movie.id" 
+                :movie="movie" 
+                @click.native="navigate(movie.id)"
+                class="movie-card"
+            />
         </div>
     </div>
 </template>
@@ -26,9 +25,13 @@
 <script>
 import { mapGetters } from 'vuex';
 import store from '../store';
+import MovieCard from './MovieCard';
 
 export default {
     name: 'movie-list',
+    components: {
+        'movie-card': MovieCard
+    },
     
     data() {
         return {
@@ -41,19 +44,27 @@ export default {
         next();
     },
 
+    methods: {
+        navigate(id) {
+            this.$router.push(`/movies/${id}`);
+            console.log('a');
+        }
+    },
+
     computed: {
         ...mapGetters(['movies']),
 
         filteredMovies() {
             return this.movies.filter(m => m.title.toLowerCase().includes(this.query.toLowerCase()));
         }
-    },
-
-    filters: {
-        getYear(date) {
-            const newDate = new Date(date);
-            return newDate.getFullYear();
-        }
     }
 }
 </script>
+
+<style scoped>
+
+.movie-card:hover {
+    cursor: pointer;
+}
+
+</style>
